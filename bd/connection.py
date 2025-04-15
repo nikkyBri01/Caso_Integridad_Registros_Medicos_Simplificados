d0808091b9276d5b340242cb8c63d49b05965c69
nikkyBri01 <nikkyarbri@gmail.com>
2025-04-15 15:30:42 -0600

    bd conexion

--- bd/connection.py
+++ bd/connection.py
@@ -3,7 +3,7 @@ import mysql.connector
 def conectar_db():
     return mysql.connector.connect(
         host="localhost",
-        user="tu_user",
-        password="tu_pass",
-        database="tu_bd"
+        user="root",
+        password="nikky01",
+        database="citas"
     )
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     
     crear_tabla()
 
-    # Agregar una nueva cita
+    # Agregar una nueva cita 
     nombre_paciente = "Juancito Pérez"
     fecha_hora = "2023-10-15 10:30:00" 
     motivo_consulta = "Consulta general"
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     cursor.execute("INSERT INTO citas (nombre_paciente, fecha_hora, motivo_consulta) VALUES (%s, %s, %s)",
                    (nombre_paciente, fecha_hora, motivo_consulta))
     
-    db.commit()  # <-- Esto guarda los cambios en la base de datos
+    db.commit()  # <-- Guarda los cambios en la base de datos
     print("Cita insertada correctamente.")
 
     cursor.close()
